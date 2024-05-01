82bb529df7801fcc4213390231a69721880f92cf
Lu Dat Luan <ludatluan1234@gmail.com>
2024-05-01 20:32:54 +0700

    update modal

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -1,6 +1,12 @@
-
-
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/editP.css">
+</head>
+<body>
 <div class="container">
     <h2>Edit Product</h2>
     <form action="{{ route('products.update', $product->id) }}" method="POST">
@@ -30,4 +36,5 @@
         <button type="submit" class="btn btn-primary">Update Product</button>
     </form>
 </div>
-
+</body>
+</html>
--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
 <body>
 <div class="container">
@@ -30,7 +32,8 @@
                 <td>${{ number_format($product->retail_price, 2) }}</td>
                 <td>{{ $product->category }}</td>
                 <td>
-                    <a href="{{ route('products.edit', $product->id) }}" class="btn btn-primary">Edit</a>
+                    <!-- Edit button triggers modal -->
+                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editProductModal-{{ $product->id }}">Edit</button>
                     <form action="{{ route('products.destroy', $product->id) }}" method="POST" style="display:inline-block;">
                         @csrf
                         @method('DELETE')
@@ -41,7 +44,49 @@
             @endforeach
         </tbody>
     </table>
-    
+
+    <!-- Modal for editing products -->
+    @foreach ($products as $product)
+    <div class="modal fade" id="editProductModal-{{ $product->id }}" tabindex="-1" role="dialog" aria-labelledby="editProductModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editProductModalLabel">Edit Product {{ $product->id }}</h5>
+                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('products.update', $product->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <div class="form-group">
+                            <label for="product_name">Product Name</label>
+                            <input type="text" class="form-control" id="product_name" name="product_name" value="{{ $product->product_name }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="barcode">Barcode</label>
+                            <input type="text" class="form-control" id="barcode" name="barcode" value="{{ $product->barcode }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="import_price">Import Price</label>
+                            <input type="number" class="form-control" id="import_price" name="import_price" value="{{ $product->import_price }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="retail_price">Retail Price</label>
+                            <input type="number" class="form-control" id="retail_price" name="retail_price" value="{{ $product->retail_price }}">
+                        </div>
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            <input type="text" class="form-control" id="category" name="category" value="{{ $product->category }}">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Update Product</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    @endforeach
 </div>
 </body>
 </html>
